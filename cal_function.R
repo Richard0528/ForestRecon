34eeaa6e1731d5ab6961532de174727a5c2757e5
Richard0528 <richardyang@D-10-16-19-162.dhcp4.washington.edu>
2017-05-31 16:35:49 -0700

    Please enter the commit message for your changes. Lines starting

--- cal_function.R
+++ cal_function.R
@@ -9,16 +9,16 @@ filepath<-"../../ForestReconstructionSim/SimResults/WithPCQDiam/Clumped/"
 load(paste(filepath,"ClumpedShape1Scale20.RData",sep = ""))
 #load(paste(filepath,"ClumpedShape",shape.vals[ii],"Scale",scale.vals[jj],".RData",sep=""))
 
-Fit_cal_func.fn<-function(ncol=4, nrow=10, sim_section=sim.density.section, 
-                          est_section=est.density.section.bia, nsim=4000, fit_cal="MSE",bySection=TRUE)
+Fit_cal_func.fn<-function(col=4, row=10, sim_section=sim.density.section, 
+                          est_section=est.density.section.bia, nsim=5000, fit_cal="MSE",bySection=TRUE)
 {
-  cur_matrix.df = data.frame(matrix(NA, ncol = ncol, nrow = nrow))
-  for (i in 1:10)
+  cur_matrix.df = data.frame(matrix(NA, ncol = col, nrow = row))
+  for (i in 1:row)
   {
     sim.tph.all = sim_section[[i]][,5]
     if(bySection)
       {
-        for (j in 1:4)
+        for (j in 1:col)
         {
             if (fit_cal == "MSE"){
               cur_matrix.df[i,j] = sum((est_section[[i]][,j] - sim.tph.all)^2)/nsim
@@ -33,14 +33,13 @@ Fit_cal_func.fn<-function(ncol=4, nrow=10, sim_section=sim.density.section,
     else
     {
       if (fit_cal == "MSE"){
-        cur_matrix.df[i,1] = sum((est_section[,i] - sim.tph.all)^2)/nsim
+        cur_matrix.df[i,col] = sum((est_section[,i] - sim.tph.all)^2)/nsim
       } else if (fit_cal == "RME"){
-        cur_matrix.df[i,1] = sum((est_section[,i] - sim.tph.all)/sim.tph.all)/nsim
+        cur_matrix.df[i,col] = sum((est_section[,i] - sim.tph.all)/sim.tph.all)/nsim
       } else if (fit_cal == "RMAE"){
-        cur_matrix.df[i,1] = sum(abs(est_section[,i] - sim.tph.all)/sim.tph.all)/nsim
+        cur_matrix.df[i,col] = sum(abs(est_section[,i] - sim.tph.all)/sim.tph.all)/nsim
       }
- #     colnames(cur_matrix.df) = c("section1", "section2", "section3", "section4")
-      
+    #colnames(cur_matrix.df) = c(paste(fit_cal,"_",sim_section,sep=""))
     }
   }
   return(cur_matrix.df)
