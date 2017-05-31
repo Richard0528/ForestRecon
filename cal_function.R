bc6aab208b217bcba321b76650bac94b8d49dca3
mkenn <mkenn@uw.edu>
2017-05-31 11:32:17 -0700

    updated cal_fun for qq, q, and t calculations

--- cal_function.R
+++ cal_function.R
@@ -9,23 +9,39 @@ filepath<-"../../ForestReconstructionSim/SimResults/WithPCQDiam/Clumped/"
 load(paste(filepath,"ClumpedShape1Scale20.RData",sep = ""))
 #load(paste(filepath,"ClumpedShape",shape.vals[ii],"Scale",scale.vals[jj],".RData",sep=""))
 
-Fit_cal_func.fn<-function(ncol=4, nrow=10, sim_section=sim.density.section, est_section=est.density.section.bia, nsim=4000, fit_cal="MSE")
+Fit_cal_func.fn<-function(ncol=4, nrow=10, sim_section=sim.density.section, 
+                          est_section=est.density.section.bia, nsim=4000, fit_cal="MSE",bySection=TRUE)
 {
   cur_matrix.df = data.frame(matrix(NA, ncol = ncol, nrow = nrow))
   for (i in 1:10)
   {
     sim.tph.all = sim_section[[i]][,5]
-    for (j in 1:4)
-    {
-        if (fit_cal == "MSE"){
-          cur_matrix.df[i,j] = sum((est_section[[i]][,j] - sim.tph.all)^2)/nsim
-        } else if (fit_cal == "RME"){
-          cur_matrix.df[i,j] = sum((est_section[[i]][,j] - sim.tph.all)/sim.tph.all)/nsim
-        } else if (fit_cal == "RMAE"){
-          cur_matrix.df[i,j] = sum(abs(est_section[[i]][,j] - sim.tph.all)/sim.tph.all)/nsim
+    if(bySection)
+      {
+        for (j in 1:4)
+        {
+            if (fit_cal == "MSE"){
+              cur_matrix.df[i,j] = sum((est_section[[i]][,j] - sim.tph.all)^2)/nsim
+            } else if (fit_cal == "RME"){
+              cur_matrix.df[i,j] = sum((est_section[[i]][,j] - sim.tph.all)/sim.tph.all)/nsim
+            } else if (fit_cal == "RMAE"){
+              cur_matrix.df[i,j] = sum(abs(est_section[[i]][,j] - sim.tph.all)/sim.tph.all)/nsim
+            }
         }
+    colnames(cur_matrix.df) = c("section1", "section2", "section3", "section4")
+    }
+    else
+    {
+      if (fit_cal == "MSE"){
+        cur_matrix.df[i,1] = sum((est_section[,i] - sim.tph.all)^2)/nsim
+      } else if (fit_cal == "RME"){
+        cur_matrix.df[i,1] = sum((est_section[,i] - sim.tph.all)/sim.tph.all)/nsim
+      } else if (fit_cal == "RMAE"){
+        cur_matrix.df[i,1] = sum(abs(est_section[,i] - sim.tph.all)/sim.tph.all)/nsim
+      }
+ #     colnames(cur_matrix.df) = c("section1", "section2", "section3", "section4")
+      
     }
   }
-  colnames(cur_matrix.df) = c("section1", "section2", "section3", "section4")
   return(cur_matrix.df)
 }
