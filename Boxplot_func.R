9476f00c0cea2771eee216409c2a354051871f7f
mkenn <mkenn@uw.edu>
2017-06-28 11:55:11 -0700

    add some plotting commands

--- Boxplot_func.R
+++ Boxplot_func.R
@@ -8,3 +8,13 @@ boxplot.fn = function(cur.est = biaglo.list[[j]],ymax=max(tph))
     points(1:10,seq(50,500,50),pch=16,col="blue",cex=2)
     legend(x="topleft",legend="True tph",pch=16,col="blue")
 }
+# 
+# > plot(seq(50,500,50),RMAE_Fit15_cal.list[,1],type="l",ylim=c(0,max(RMAE_Fit15_cal.list)))
+# > for(i in 2:ncol(MSE_Fit_cal.list))
+#   + lines(seq(50,500,50),RMAE_Fit15_cal.list[,i],lty=i)
+
+# > plot(1:ncol(MSE_Fit_cal.list),MSE_Fit_cal.list[1,])
+# > plot(1:ncol(MSE_Fit_cal.list),MSE_Fit_cal.list[2,])
+# > plot(1:ncol(MSE_Fit_cal.list),MSE_Fit_cal.list[3,])
+# > plot(1:ncol(MSE_Fit_cal.list),MSE_Fit_cal.list[4,])
+# > plot(1:ncol(MSE_Fit_cal.list),RMAE_Fit_cal.list[4,])
